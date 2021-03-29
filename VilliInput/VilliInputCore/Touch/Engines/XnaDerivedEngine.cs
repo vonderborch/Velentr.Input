940f863482b4af9b7f6f6f4d17e12705e77befdb
Christian Webber <ricky.webber@hotmail.com>
2021-03-28 23:01:23 -0400

    Minor fix

--- VilliInput/VilliInputCore/Touch/Engines/XnaDerivedEngine.cs
+++ VilliInput/VilliInputCore/Touch/Engines/XnaDerivedEngine.cs
@@ -14,45 +14,52 @@ namespace VilliInput.Touch.Engines
 
         public TouchCollection CurrentState { get; private set; }
 
-        public TouchEngines EnabledTouchEngine { get; private set; }
-
         public override void Setup()
         {
             Capabilities = TouchPanel.GetCapabilities();
+            Gestures = new Dictionary<GestureType, List<Gesture>>(Enum.GetNames(typeof(GestureType)).Length);
             if (Capabilities.IsConnected)
             {
                 TouchPanelConnected = true;
                 MaxTouchPoints = Capabilities.MaximumTouchCount;
 
                 CurrentState = new TouchCollection();
-                Gestures = new Dictionary<GestureType, List<Gesture>>(Enum.GetNames(typeof(GestureType)).Length);
-
                 TouchPanel.EnabledGestures = Villi.System.Settings.EnabledTouchGestures;
             }
         }
 
         public override void Update()
         {
-            CurrentState = TouchPanel.GetState();
-
-            // Update gestures...
-            foreach (var gesture in Gestures)
+            if (TouchPanelConnected)
             {
-                gesture.Value.Clear();
-            }
+                CurrentState = TouchPanel.GetState();
 
-            var id = 0;
-            while (TouchPanel.IsGestureAvailable)
-            {
-                var gesture = TouchPanel.ReadGesture();
-                var type = TouchGestureMapping.XnaToInternal[gesture.GestureType];
-
-                if (!Gestures.ContainsKey(type))
+                // Update gestures...
+                if (Gestures.Count == 0)
                 {
-                    Gestures.Add(type, new List<Gesture>());
+                    Gestures = new Dictionary<GestureType, List<Gesture>>(Enum.GetNames(typeof(GestureType)).Length);
+                }
+                else
+                {
+                    foreach (var gesture in Gestures)
+                    {
+                        gesture.Value.Clear();
+                    }
                 }
 
-                Gestures[type].Add(new Gesture(id++, gesture.Position, gesture.Position2, gesture.Delta, gesture.Delta2, type, gesture.Timestamp));
+                var id = 0;
+                while (TouchPanel.IsGestureAvailable)
+                {
+                    var gesture = TouchPanel.ReadGesture();
+                    var type = TouchGestureMapping.XnaToInternal[gesture.GestureType];
+
+                    if (!Gestures.ContainsKey(type))
+                    {
+                        Gestures.Add(type, new List<Gesture>());
+                    }
+
+                    Gestures[type].Add(new Gesture(id++, gesture.Position, gesture.Position2, gesture.Delta, gesture.Delta2, type, gesture.Timestamp));
+                }
             }
         }
 
